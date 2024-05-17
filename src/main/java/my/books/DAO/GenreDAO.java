ae13cce46a66e50ab735807d4b006bb7c2486f25
alexsanya001 <alexsanya001@gmail.com>
2024-05-17 12:58:06 +0300

    GenreDAO realised

--- src/main/java/my/books/DAO/GenreDAO.java
+++ src/main/java/my/books/DAO/GenreDAO.java
@@ -1,5 +1,6 @@
 package my.books.DAO;
 
+import my.books.models.Book;
 import my.books.models.Genre;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -15,8 +16,22 @@ public class GenreDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Genre> findAll(){
+    public List<Genre> findAll() {
         return jdbcTemplate.query("SELECT * FROM genres", new GenreMapper());
+    }
+
+    public Genre findByTitle(String title) {
+        return jdbcTemplate.query("SELECT * FROM genres WHERE g_title=?",
+                new GenreMapper(), title).stream().findAny().orElse(null);
+    }
+
+    public Iterable<Book> genreBooks(String g_title) {
+        return jdbcTemplate.query("SELECT b.id, b.b_title, a.name, g.g_title " +
+                        "FROM books b " +
+                        "JOIN authors a ON b.author_id = a.id " +
+                        "JOIN genres g ON b.genre_id = g.id " +
+                        "WHERE b.genre_id = (SELECT id FROM genres WHERE g_title = ?)",
+                new BookMapper(), g_title);
     }
 
 }
---
+++ src/main/java/my/books/controllers/GenreController.java
@@ -0,0 +1,33 @@
+package my.books.controllers;
+
+
+import my.books.DAO.GenreDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/genres")
+public class GenreController {
+
+    private final GenreDAO genreDAO;
+
+    public GenreController(GenreDAO genreDAO) {
+        this.genreDAO = genreDAO;
+    }
+
+    @GetMapping
+    public String allGenres(Model model){
+        model.addAttribute("genres", genreDAO.findAll());
+        return "genres/allGenres";
+    }
+
+    @GetMapping("{g_title}")
+    public String aGenre(@PathVariable("g_title") String g_title, Model model){
+        model.addAttribute("a_genre", genreDAO.findByTitle(g_title));
+        model.addAttribute("genreBooks", genreDAO.genreBooks(g_title));
+        return "genres/aGenre";
+    }
+}
